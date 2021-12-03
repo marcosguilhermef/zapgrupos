d3615a08c2b6253e28e1c5a5ab861fe1de208110
micos <marcosguilhermefh@gmail.com>
2021-12-03 15:52:46 -0300

    og:image:width og:image:heigth

--- app/Http/Controllers/PorCategoria.php
+++ app/Http/Controllers/PorCategoria.php
@@ -26,6 +26,8 @@ class PorCategoria extends Controller
     <meta property=\"og:title\" content=\"".$conf['title']."\" />\n
     <meta property=\"og:description\" content=\"".@$dados[0]["descricao"]."\" />\n
     <meta property=\"og:image\" content=\"".$img."\" />\n
+    <meta property=\"og:image:width\" content=\"600px\" />\n
+    <meta property=\"og:image:height \" content=\"600px\" />\n
     <meta property=\"og:site_name\" content=\"Zapgrupos\" />\n
     <meta itemprop=\"name\" content=\"".$conf['title']."\">\n
     <meta itemprop=\"description\" content=\"".@$dados[0]["descricao"]."\">\n
