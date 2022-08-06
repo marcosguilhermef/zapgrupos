0af6b420d69ef3ced00f20d6491f1e4a2441fb7e
marcosguilhermef <marcosguilhermefh@gmail.com>
2022-08-05 23:09:13 -0300

    ajutes meta

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -14,7 +14,30 @@ class InicioController extends Controller
         $conf["titulo_2"] = 'Grupos recem criados';
         $conf["api"]       = 'api/recentes';
         $conf["meta"]      ='
+        <meta name="description" content="Encontre grupos de whatsapp e telegram de acordo com o seu interesse. Temos mais de 20 categorias de grupos para os mais diversos interesses. Você também pode cadastrar o seu grupo para que mais pessoas entrem nos seus grupos."  />
+        <meta property="article:tag" content="zaps, whatsapp, whats, zapzapgrupos, android, grupos, zapgrupos, zapzap, zap, groups, imagens, videos, dicas, som, tudo whats, tudo facebook, redes sociais, noticias, apps" />
+        <link rel="canonical" href="https://zapgrupos.xyz/" />
+        <meta name="robots" content="index, follow" />
+        <meta name="lang" content="pt_BR" />
+        <meta name="ms.locate" content="pt_BR" />
+        <meta name="category" content="publicidade" />
+        <meta name="resource-type" content="document" />
+        <meta name="classification" content="Internet" />
+        <meta name="distribution" content="Global" />
+        <meta name="rating" content="General" />
+        <meta name="doc-class" content="Completed" />
+        <meta name="doc-rights" content="Public" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="ZapGrupos - Encontre Grupos de WhatsApp e Telegram" />
+        <meta property="og:description" content="Encontre grupos de whatsapp e telegram de acordo com o seu interesse. Temos mais de 20 categorias de grupos para os mais diversos interesses. Você também pode cadastrar o seu grupo para que mais pessoas entrem nos seus grupos." />
+        <meta property="og:url" content="https://zapgrupos.xyz/" />
+        <meta property="og:image" content="https://zapgrupos.xyz/img/logo/logo.png" />
+        <meta property="og:site_name" content="Zapgrupos" />
+        <meta http-equiv="Content-Language" content="pt-br" />
+        <meta name="keywords" CONTENT="zaps, whatsapp, whats, zapzapgrupos, android, grupos, zapgrupos, zapzap, zap, groups, grupos, videos, memes, imagens, mp4, fotos, imagens, instagram, recados para facebook, facebook, imagens facebook, capas facebook, figuras, imagens de exibição, avatar, papéis de parede, wallpaper, emoticons, ícones, webobjects, categorias jogos, games, cinema, amor, filmes, desenhos, fun, esportes, windows, feminino, beijos, montagens, mulheres, artistas, música, home page, webmaster, design, programação, tecnologia e mais" />
+        <meta name="revisit-after" content="1 Days" />
         <meta name="description" content="Encontre grupos de whatsapp e telegram de acordo com o seu interesse. Temos mais de 20 categorias de grupos para os mais diversos interesses. Você também pode cadastrar o seu grupo para que mais pessoas entrem nos seus grupos." />
+        
         <script type="application/ld+json">
                {
               "@context": "https://schema.org",
@@ -39,6 +62,6 @@ class InicioController extends Controller
             }
             }
             </script>';
-        return Inertia::render('Externo/Inicio', $conf);
+        return Inertia::render("Externo/Inicio", $conf);
     }
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,6 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta name="language" content="pt">
-    <meta name="facebook-domain-verification" content="ct721bzana98oh0y6zuftib94fa5k9" />
-    <meta name="propeller" content="e91767eddc63b2a7e496835245fe4e98">
-    <meta property="fb:app_id" content="1279398252524269" />
     <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
@@ -18,7 +15,8 @@
     {!! isset($page['props']['meta']) ? $page['props']['meta'] : '' !!}
     <title>{{ isset($page['props']['title']) ? $page['props']['title'] : 'Zapgrupos'  }}</title>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('/js/app.js') }}" defer></script>    
+    <script src="{{ mix('/js/app.js') }}" defer></script> 
+       
     @isset($page['props']['role'])
       @if($page['props']['role'] === 'user')
         <script data-ad-client="ca-pub-8817634033676287" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
