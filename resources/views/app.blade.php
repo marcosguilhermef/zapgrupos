30b7910e5fb1bbc5ed18d72b6517970dd611f6ec
marcosguilhermef <marcosguilhermefh@gmail.com>
2022-11-03 18:51:58 -0300

    ads after </body>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,7 +16,12 @@
     <title>{{ isset($page['props']['title']) ? $page['props']['title'] : 'Zapgrupos'  }}</title>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script> 
-       
+  </head>
+  <body>
+    {!! isset($page['props']['facebook']) ? $page['props']['facebook'] : '' !!}
+    @inertia
+    {!! isset($script) ? $script : '' !!}
+
     @isset($page['props']['role'])
       @if($page['props']['role'] === 'user')
         {{-- <script data-ad-client="ca-pub-8817634033676287" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> --}}
@@ -54,10 +59,6 @@
       </script>
 
     @endempty
-  </head>
-  <body>
-    {!! isset($page['props']['facebook']) ? $page['props']['facebook'] : '' !!}
-    @inertia
-    {!! isset($script) ? $script : '' !!}
+    
   </body>
 </html>
